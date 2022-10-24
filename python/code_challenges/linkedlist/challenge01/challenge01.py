e2afd90db168d3d7c7c535039a813da315a5b557
mkhader96 <mkhader996@gmail.com>
2022-10-24 12:03:27 +0000

    Updated Solution

--- python/code_challenges/linkedlist/challenge01/challenge01.py
+++ python/code_challenges/linkedlist/challenge01/challenge01.py
@@ -1,15 +1,13 @@
 
 
 class Node:
-    def __init__(self,value):
-        self.value = value
-        self.next = None
+     def __init__(self, x):
+         self.val = x
+         self.next = None
 class LinkedList:
     def __init__(self):
         self.head = None
-
     def append(self, node):
-        '''A function that adds a new node with the given value to the end of the list'''
         if self.head is None:
             self.head = node
         else:
@@ -19,38 +17,19 @@ class LinkedList:
             current.next = node
 
     def printAll(self):
-        '''A function that returns all the values in the linked list'''
-        elements = []
+        elements=[]
         if self.head is None:
-            print("The linked list is empty")
+            return("The linked list is empty")
         else:
             current = self.head
             while current is not None:
-                elements.append(current.value)
+                elements.append(current.val)
                 current = current.next
-        return elements
+            return elements
 
-    def delete(self, value):
-        '''A function that takes any value as an argument and removes the first node with that value from the list'''
-        current = self.head
-        if current.value == value:
-            self.head = current.next
-        else:
-            while current.next is not None:
-                if current.next.value == value:
-                    current.next = current.next.next
-                else:
-                    current = current.next
+def deleteNode(node):
+    if not node:
+        return
+    node.val = node.next.val
+    node.next = node.next.next
 
-
-def deleteNodefromlist(list,value):
-    '''A function that takes a list and a value, converts the list to a linekd list and remove the node with the given value. Then returns the linked list as a list'''
-    ll = LinkedList()
-    for i in list:
-        ll.append(Node(i))
-    ll.delete(value)
-    return ll.printAll()
-
-
-
-    
