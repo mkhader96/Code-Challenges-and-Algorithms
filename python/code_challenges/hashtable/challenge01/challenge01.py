9205122955cec10874fbf487b23f7b4691953bb7
mkhader96 <mkhader996@gmail.com>
2022-12-13 13:14:31 +0000

    Second Solve

--- python/code_challenges/hashtable/challenge01/challenge01.py
+++ python/code_challenges/hashtable/challenge01/challenge01.py
@@ -1,22 +1,24 @@
-class Tree:
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
-def Solution(root, k):
-    '''A function that takes a BST and a value and returns true if there are 2 numbers in the tree that can be summed to equal the value entered'''
-    tree_values = []
 
-    def Two_Sum_BST(node):
-        if not node:
-            return False
-        diff = k - node.val
-        if diff in tree_values:
+def Solution(TreeNode, k):
+    Stack, HashTable = [TreeNode], set()
+    while Stack:
+        node = Stack.pop()
+        if k - node.val in HashTable: 
             return True
-        else:
-            tree_values.append(node.val)
-        return Two_Sum_BST(node.left) or Two_Sum_BST(node.right)
+        HashTable.add(node.val)
+        if node.left: 
+            Stack.append(node.left)
+        if node.right: 
+            Stack.append(node.right)
+            
+    return False
 
-    return Two_Sum_BST(root)
+
+
--- python/code_challenges/hashtable/challenge01/test_challenge01.py
+++ python/code_challenges/hashtable/challenge01/test_challenge01.py
@@ -1,37 +1,37 @@
 import pytest
-from challenge01 import Solution, Tree
+from challenge01 import  TreeNode, Solution
 
 
 def test_sum_bst():
-    tree1 = Tree(7)
-    tree1.left = Tree(2)
-    tree1.left.left = Tree(1)
-    tree1.left.right = Tree(5)
-    tree1.right = Tree(9)
-    tree1.right.right = Tree(14)
-    actual = Solution(tree1, 3)
+    tree1 = TreeNode(7)
+    tree1.left = TreeNode(2)
+    tree1.left.left = TreeNode(1)
+    tree1.left.right = TreeNode(5)
+    tree1.right = TreeNode(9)
+    tree1.right.right = TreeNode(14)
+    actual = Solution(tree1,7)
     expected = True
     assert actual == expected
 
 
 def test1_sum_bst():
-    tree1 = Tree(7)
-    tree1.left = Tree(2)
-    tree1.left.left = Tree(1)
-    tree1.left.right = Tree(5)
-    tree1.right = Tree(9)
-    tree1.right.right = Tree(14)
+    tree1 = TreeNode(7)
+    tree1.left = TreeNode(2)
+    tree1.left.left = TreeNode(1)
+    tree1.left.right = TreeNode(5)
+    tree1.right = TreeNode(9)
+    tree1.right.right = TreeNode(14)
     actual = Solution(tree1, 4)
     expected = False
     assert actual == expected
 
 def test2_sum_bst():
-    tree1 = Tree(8)
-    tree1.left = Tree(4)
-    tree1.left.left = Tree(2)
-    tree1.left.right = Tree(6)
-    tree1.right = Tree(15)
-    tree1.right.right = Tree(16)
+    tree1 = TreeNode(8)
+    tree1.left = TreeNode(4)
+    tree1.left.left = TreeNode(2)
+    tree1.left.right = TreeNode(6)
+    tree1.right = TreeNode(15)
+    tree1.right.right = TreeNode(16)
     actual = Solution(tree1, 21)
     expected = True
     assert actual == expected
