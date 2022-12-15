b47a960872899be1455a178a03f06bd5bfb9cbfc
Mohammad Khader <109700344+mkhader96@users.noreply.github.com>
2022-12-15 15:05:39 +0300

    Update challenge02.py

--- python/code_challenges/hashtable/challenge02/challenge02.py
+++ python/code_challenges/hashtable/challenge02/challenge02.py
@@ -1,6 +1,6 @@
 def find_first_repeated_word(string):
     '''
-    A function that takes a string and then splits it to a list,the function also creates a hashset. Then we loop through the list, if the 
+    A function that takes a string and then splits it to a list,the function also creates a hashset. Then we loop through the list, if the word is available in the set already then the word is repeated so we return the word, else if the word is not available then we add it to the set to check it again. If there is no repeated words then return "No Repetition"
     '''
     list = string.split()
     hash = set()
