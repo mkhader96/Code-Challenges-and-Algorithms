3a5b6b3c05fc07d05083280b79e7782e0e918da8
mkhader96 <mkhader996@gmail.com>
2022-12-17 14:54:47 +0000

    Graph Code Challange

---
+++ python/code_challenges/graph/challenge01/challenge01.py
@@ -0,0 +1,77 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+    
+    def __str__(self):
+        return self.value
+
+class Edge:
+    def __init__(self,vertex,weight=0):
+        self.vertex = vertex
+        self.weight = weight
+
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_node(self,value):
+        new_vertex = Node(value)
+        self.adj_list[new_vertex] = []
+        return new_vertex
+
+    def add_edge(self,node1,node2,weight=0):
+        
+        if not node1 in self.adj_list.keys():
+            return("this node does not exist")
+
+        if not node2 in self.adj_list.keys():
+            return("this node does not exist")
+        
+        edge1 = Edge(node2,weight)
+        self.adj_list[node1].append(edge1)
+
+        edge2 = Edge(node1,weight)
+        self.adj_list[node2].append(edge2)
+
+    def breadth_first_traversal(self, input_value):
+        visited = []
+        queue = [input_value]
+        while queue:
+            current_node = queue.pop(0)
+            if current_node.value not in visited:
+                visited.append(current_node.value)
+                for edge in self.adj_list[current_node]:
+                    queue.append(edge.vertex)
+        return visited
+
+
+if __name__ == "__main__":
+    gr = Graph()
+    a = gr.add_node('A')
+    b = gr.add_node('B')
+    c = gr.add_node('C')
+    d = gr.add_node('D')
+    e = gr.add_node('E')
+    f = gr.add_node('F')
+    g = gr.add_node('G')
+    h = gr.add_node('H')
+    i = gr.add_node('I')
+    k = gr.add_node('K')
+
+
+    gr.add_edge(a, c)
+    gr.add_edge(a, e)
+    gr.add_edge(a, b)
+    gr.add_edge(e, f)
+    gr.add_edge(e, g)
+    gr.add_edge(e, d)
+    gr.add_edge(f, i)
+    gr.add_edge(h, k)
+    gr.add_edge(b, d)
+    gr.add_edge(g, h)
+    gr.add_edge(i, k)
+    gr.add_edge(c, f)
+    gr.add_edge(f, h)
+
+
+    print(gr.breadth_first_traversal(c))
---
+++ python/code_challenges/graph/challenge01/test_challenge01.py
@@ -0,0 +1,73 @@
+import pytest
+from challenge01 import Graph
+
+def test_graph_breadth_first_traversal():
+    g = Graph()
+    node1 = g.add_node(1)
+    node2 = g.add_node(2)
+    node3 = g.add_node(3)
+    node4 = g.add_node(4)
+    g.add_edge(node1, node2)
+    g.add_edge(node1, node3)
+    g.add_edge(node2, node3)
+    g.add_edge(node3, node4)
+    actual = [1, 2, 3, 4]
+    expected = g.breadth_first_traversal(node1)
+    assert actual == expected
+
+
+def test2_graph_breadth_first_traversal():
+    g = Graph()
+    node1 = g.add_node(1)
+    node2 = g.add_node(2)
+    node3 = g.add_node(3)
+    node4 = g.add_node(4)
+    g.add_edge(node1, node2)
+    g.add_edge(node1, node3)
+    g.add_edge(node2, node3)
+    g.add_edge(node3, node4)
+    actual = [3, 1, 2, 4]
+    expected = g.breadth_first_traversal(node3)
+    assert actual == expected
+
+def test3_graph_breadth_first_traversal():
+    g = Graph()
+    node_a = g.add_node("a")
+    node_b = g.add_node("b")
+    node_c = g.add_node("c")
+    node_d = g.add_node("d")
+    node_e = g.add_node("e")
+    node_f = g.add_node("f")
+    node_g = g.add_node("g")
+    g.add_edge(node_a, node_b)
+    g.add_edge(node_a, node_c)
+    g.add_edge(node_b, node_c)
+    g.add_edge(node_c, node_d)
+    g.add_edge(node_c, node_e)
+    g.add_edge(node_d, node_f)
+    g.add_edge(node_e, node_f)
+    g.add_edge(node_f, node_g)
+    actual = g.breadth_first_traversal(node_c)
+    expected = ["c", "a", "b", "d", "e", "f", "g"]
+    assert actual == expected
+
+def test4_graph_breadth_first_traversal():
+    g = Graph()
+    node_a = g.add_node("a")
+    node_b = g.add_node("b")
+    node_c = g.add_node("c")
+    node_d = g.add_node("d")
+    node_e = g.add_node("e")
+    node_f = g.add_node("f")
+    node_g = g.add_node("g")
+    g.add_edge(node_a, node_b)
+    g.add_edge(node_a, node_c)
+    g.add_edge(node_b, node_c)
+    g.add_edge(node_c, node_d)
+    g.add_edge(node_c, node_e)
+    g.add_edge(node_d, node_f)
+    g.add_edge(node_e, node_f)
+    g.add_edge(node_f, node_g)
+    actual = g.breadth_first_traversal(node_g)
+    expected = ["g", "f", "d", "e", "c", "a", "b"]
+    assert actual == expected
